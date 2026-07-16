9e94c69b6b32dc43fe83f869c90790ee3c0251e1
guiomenezes <guilherme.menezes@outlook.com>
2026-07-16 19:01:58 -0300

    CORSMiddleware was added in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,10 +3,19 @@ from app.routers import guests, rooms, reservations, payments, reports
 from app.routers import auth
 from app import models
 from app.database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title='TropicBook_API')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials = True, 
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 app.include_router(guests.router)
 app.include_router(rooms.router)
