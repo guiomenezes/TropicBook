2b5626fa2941765b669fbb1b97777be25c278c95
guiomenezes <guilherme.menezes@outlook.com>
2026-07-16 10:07:42 -0300

    Login html file and auth js configured.

---
+++ frontend/js/auth.js
@@ -0,0 +1,43 @@
+const form = document.getElementById("login-form");
+
+form.addEventListener("submit", async (event) => {
+
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+
+    const password = document.getElementById("password").value;
+
+    const response = await fetch(
+        "http://127.0.0.1:8000/auth/login",
+        {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                email,
+                password
+            })
+        }
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+
+        document.getElementById("error-message").textContent =
+            data.detail;
+
+        return;
+    }
+
+    localStorage.setItem(
+        "token",
+        data.access_token
+    );
+
+    alert("Login realizado com sucesso!");
+});
